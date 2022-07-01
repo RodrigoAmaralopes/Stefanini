00df54a7dbd59bec2f58090a1083b933b9785812
Rodrigo Amaral <108424004+RodrigoAmaralopes@users.noreply.github.com>
2022-07-01 16:37:52 -0300

    Add files via upload

--- BackEnd/Java/Basico/Estrutura-de-Controle/DoWhile.java
+++ BackEnd/Java/Basico/Estrutura-de-Controle/DoWhile.java
@@ -1,5 +1,16 @@
 package br.com.stefanini.cursobasico.estruturacontrole;
 
 public class DoWhile {
-
+public static void main(String[] args) {
+	
+	
+	int count = 101;
+	
+	do {
+		
+		System.out.println(count);
+		count++;
+	} while (count <100);
+	
+}
 }
