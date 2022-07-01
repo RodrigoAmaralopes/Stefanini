9365af6e95b83e3041e26e58e57450dea46d9235
Rodrigo Amaral <108424004+RodrigoAmaralopes@users.noreply.github.com>
2022-07-01 14:13:56 -0300

    Add files via upload
    Primeiras aulas feitas com a IDE Eclipse

---
+++ BackEnd/Java/Aprendendo-Eclipse/Aula01.java
@@ -0,0 +1,13 @@
+package br.com.stefanini.aprendendocomeclipse.aula;
+
+public class Aula01 {
+	
+public static void main(String[] args) {
+	
+	Aula02 aula = new Aula02();
+	System.out.println(aula);
+	
+	
+	
+}
+}
---
+++ BackEnd/Java/Aprendendo-Eclipse/Aula02.java
@@ -0,0 +1,9 @@
+package br.com.stefanini.aprendendocomeclipse.aula;
+
+public class Aula02 {
+	
+	public static void main(String[] args) {
+		System.out.println();
+	}
+
+}
