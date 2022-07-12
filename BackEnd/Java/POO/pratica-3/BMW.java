5891d6abe70b4e642ece9def4d93cc42c74a8668
Rodrigo Amaral <108424004+RodrigoAmaralopes@users.noreply.github.com>
2022-07-12 16:46:22 -0300

    Add files via upload

---
+++ BackEnd/Java/POO/pratica-3/BMW.java
@@ -0,0 +1,8 @@
+package br.com.stefanini.orientacaoaobjetos.pratica3;
+public class BMW extends Carro {
+    @Override
+    public double calculcarTaxaAceleracao(double vel_final, double tempo_final) {
+        System.out.println("BMW Sobreescrita");
+        return vel_final / tempo_final;
+    }
+}
