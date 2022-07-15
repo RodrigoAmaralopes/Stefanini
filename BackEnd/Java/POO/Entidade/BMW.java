5fde3611a8d517d6ba1b95820e8632eac6c8dc11
Rodrigo Amaral <108424004+RodrigoAmaralopes@users.noreply.github.com>
2022-07-15 17:03:49 -0300

    Add files via upload

--- BackEnd/Java/POO/Entidade/BMW.java
+++ BackEnd/Java/POO/Entidade/BMW.java
@@ -1,20 +1,9 @@
 package br.com.stefanini.orientacaoaobjetos.entidade;
 
-import br.com.stefanini.orientacaoaobjetos.Carro;
-
 public class BMW extends Carro {
-	
-	public BMW () {
-		this.setMarca("BMW");
-		
-		
-	}
-	
-	@Override
-	public String acelerar() {
-		// TODO Auto-generated method stub
-		return " A BMW pode fazer de 0 " + super.acelerar()+ " em 5s" ;
-	}
-	
-
+    @Override
+    public double calcularTaxaAceleracao(double vel_final, double tempo_final) {
+        System.out.println("BMW Sobreescrita");
+        return vel_final / tempo_final;
+    }
 }
