ba536c43b6378a7866cfcef4270bc520d6841361
Rodrigo Amaral <108424004+RodrigoAmaralopes@users.noreply.github.com>
2022-07-15 15:57:18 -0300

    Add files via upload

--- BackEnd/Java/POO/pratica-2/BMW.java
+++ BackEnd/Java/POO/pratica-2/BMW.java
@@ -1,9 +1,9 @@
 package br.com.stefanini.orientacaoaobjetos.pratica2;
 
 public class BMW extends Carro {
-    @Override
-    public double calcularTaxaAceleracao(double vel_final, double tempo_final) {
-        System.out.println("Sobreescrita");
-        return vel_final / tempo_final;
-    }
+	@Override
+	public double calcularTaxaAceleracao(double vel_final, double tempo_final) {
+		System.out.println("Sobreescrita");
+		return vel_final / tempo_final;
+	}
 }
--- BackEnd/Java/POO/pratica-2/Principal.java
+++ BackEnd/Java/POO/pratica-2/Principal.java
@@ -1,31 +1,48 @@
 package br.com.stefanini.orientacaoaobjetos.pratica2;
 
-
 public class Principal {
-    public static void main(String[] args){
-        Carro bmw = new Carro();
-        Carro mercedes = new Carro("Mercedes","guingu");
-        BMW bmw2 = new BMW();
 
-        bmw2.calcularTaxaAceleracao(100.4, 5);
+	/*
+	 * 
+	 * 
+	 * MODIFICADORES DE ACESSO
+	 * 
+	 * DEFAULT => NAO UMA PALAVRA CHAVE (Palavra reservada). própria classe de um
+	 * mesmo pacote PUBLIC => PERMITE ACESSO DE QUALQUER LUGAR, PRÓPRIA CLASSE,
+	 * CLASSE DO MESMO PACOTE, SUBCLASSES (EXTENDS), TODAS AS DEMAIS CLASSES PRIVATE
+	 * =>ACESSÍVEL PRÓPRIA CLASSE PROTECTED => ACESSIVEL PRÓPRIA CLASSE, CLASSE
+	 * MESMO PACOTE, SUBCLASSES
+	 * 
+	 * 
+	 * 
+	 */
 
-        bmw.setMarca("BMW");
-        bmw.setModelo("M3");
-        bmw.setKm_litro(7.5);
-        //bmw.aceleracao = 50;
-        bmw.setVelocidade_max(280);;
-        
-        System.out.println(mercedes.getMarca());
-        System.out.println(mercedes.getModelo());
-        System.out.println(bmw.getKm_litro());
-        System.out.println(bmw.getAceleracao());
-        System.out.println(bmw.getVelocidade_max());
+	public static void main(String[] args) {
 
-        bmw.acelerar();
-        bmw.frear();
+		Carro bmw = new Carro();
+		Carro mercedes = new Carro("Mercedes", "C180");
+		BMW bmw2 = new BMW();
 
-        System.out.println(bmw.calcularTaxaAceleracao(bmw.getVelocidade_max(), 0, 10, 0));
-        System.out.println(bmw.calcularTaxaAceleracao(bmw.getVelocidade_max(), 10));
-        System.out.println(bmw.calcularTaxaAceleracao(100, 5));
-    }
+		bmw2.calcularTaxaAceleracao(100.4, 5);
+
+		bmw.setMarca("BMW");
+		bmw.setModelo("M3");
+		bmw.setKm_litro(7.5);
+		// bmw.aceleracao = 50;
+		bmw.setVelocidade_max(280);
+		;
+
+		System.out.println(mercedes.getMarca());
+		System.out.println(mercedes.getModelo());
+		System.out.println(bmw.getKm_litro());
+		System.out.println(bmw.getAceleracao());
+		System.out.println(bmw.getVelocidade_max());
+
+		bmw.acelerar();
+		bmw.frear();
+
+		System.out.println(bmw.calcularTaxaAceleracao(bmw.getVelocidade_max(), 0, 10, 0));
+		System.out.println(bmw.calcularTaxaAceleracao(bmw.getVelocidade_max(), 10));
+		System.out.println(bmw.calcularTaxaAceleracao(100, 5));
+	}
 }
